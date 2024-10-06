206f34c95c51db3b29094e390743faf414b38dda
Nurzhan Pernebayev <nur.pernebaev@gmail.com>
2024-10-06 23:21:11 +0500

    added Circle class

--- MIndbox.ShapeAreaLibrary/Figures/Circle.cs
+++ MIndbox.ShapeAreaLibrary/Figures/Circle.cs
@@ -4,9 +4,15 @@ namespace MIndbox.ShapeAreaLibrary.Figures;
 
 public class Circle : IShape
 {
-    
+    public double Radius { get; private set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
     public double CalculateArea()
     {
-        throw new NotImplementedException();
+        return Math.PI * Math.Pow(Radius, 2);
     }
 }
