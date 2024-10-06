ef1b614bfa85a820866c0054bd067c68018223ea
Nurzhan Pernebayev <nur.pernebaev@gmail.com>
2024-10-06 23:29:37 +0500

    added Triangle class

--- MIndbox.ShapeAreaLibrary/Figures/Triangle.cs
+++ MIndbox.ShapeAreaLibrary/Figures/Triangle.cs
@@ -4,8 +4,32 @@ namespace MIndbox.ShapeAreaLibrary.Figures;
 
 public class Triangle : IShape
 {
+    public double SideA { get; private set; }
+    public double SideB { get; private set; }
+    public double SideC { get; private set; }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        // Все стороны треугольника должны быть больше нуля
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+        {
+            throw new ArgumentException("All sides of a triangle must be greater than zero.");
+        }
+
+        // Треугольник не может иметь все стороны равные нулю.
+        if (sideA == 0 && sideB == 0 && sideC == 0)
+        {
+            throw new ArgumentException("A triangle cannot have all sides equal to zero.");
+        }
+
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
     public double CalculateArea()
     {
-        throw new NotImplementedException();
+        double s = (SideA + SideB + SideC) / 2;
+        return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
     }
 }
